8b8cd7b461f217ca574b043d7c4fa4d630428310
Alexkezz <alex391969@gmail.com>
2023-09-19 22:28:30 +0200

    createRoutineStyle

--- appView/src/app/pages/create-new-routine/create-new-routine.component.ts
+++ appView/src/app/pages/create-new-routine/create-new-routine.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-create-new-routine',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CreateNewRoutineComponent {
 
+  constructor(private api : ApiService){
+
+  }
+
+  ngOnInit(){
+    
+  }
 }
